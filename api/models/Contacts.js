10bee328d790e5f7214143d90b75276d50c6972c
Arbaz yousaf <arbaz.k480@yahoo.com>
2024-09-20 00:51:03 +0500

    " "

--- api/models/Contacts.js
+++ api/models/Contacts.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const ContactSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: false
+    },
     name: {
         type: String,
         required: true
